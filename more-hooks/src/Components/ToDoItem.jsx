9adc780fb0ab8983fbe13eb6a8b0b0b35f4fd50d
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2024-05-08 15:36:08 +0530

    more-hooks

---
+++ more-hooks/src/Components/ToDoItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ToDoItem = ({item: {data,isHidden} ,index,dispatch}) => {
+    console.log("isHidden: ", isHidden);
+  return (
+    <div style={{
+        backgroundColor: "navy",
+        width: "90vw",
+        padding: "5px",
+        margin: "20px",
+        filter: `${isHidden?"blur(2px)":"blur(0px)"}`
+    }}>
+        <h3>{isHidden ? "This Content is Hidden": data}</h3>
+        <button onClick={()=>{
+            dispatch({type: "CHANGE_ISHIDDEN" , payload: index})
+        }}>Toggle</button>
+    </div>
+  )
+}
+
+export default ToDoItem
